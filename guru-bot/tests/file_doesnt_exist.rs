91db58b0a49e464868042960b98f14befa4dfa79
Shomari Prince <nyusternie@sdot.io>
2022-09-18 07:38:28 -0400

    Fix test.

--- guru-bot/tests/file_doesnt_exist.rs
+++ guru-bot/tests/file_doesnt_exist.rs
@@ -6,10 +6,12 @@ use std::process::Command; // Run programs
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("guru")?;
 
-    cmd.arg("bar").arg("bin/test.txt");
-    // cmd.assert()
-    //     .failure()
-    //     .stderr(predicate::str::contains("could not read file"));
+    cmd.arg("bar")
+        .arg("bin/test.txt");
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
 
     Ok(())
 }
